6e1274f48a582e0ecea003434fe22ac84444e25a
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-24 21:09:36 +0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,9 +6,9 @@ import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <LinkedIn size={20} />,
+		icon: <Linkedin size={20} />,
 		href: "https://www.linkedin.com/in/jeetdekivadia/",
-		label: "LinkedIn",
+		label: "Linkedin",
 		handle: "@jeetdekivadia",
 	},
 	{
