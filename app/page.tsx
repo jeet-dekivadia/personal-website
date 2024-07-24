12b37f7d4db0c14b7ea43f37a81f506f30d67061
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-24 15:26:23 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-lg text-zinc-500 ">
           CS freshman at Georgia Tech
         </h2>
       </div>
